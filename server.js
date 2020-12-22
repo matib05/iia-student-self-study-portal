ae07340e61ed00c95f97957626003c23acc792ff
Matib Ahmad <matib05@gmail.com>
2020-12-22 02:08:18 -0500

    removing server serving index.html route only

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static( __dirname + '/frontend/dist/frontend/index.html'));
+app.use(express.static( __dirname + '/frontend/dist/frontend'));
 
 app.use('/api/user', userRouter);
 app.use('/api/assignments', assignmentRouter);
